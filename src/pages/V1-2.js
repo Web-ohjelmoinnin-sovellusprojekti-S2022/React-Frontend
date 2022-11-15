c4adeb9d1fd8d8541e7744faf5823aed43c7bf6f
Frostedlol <t0yrvi00@students.oamk.fi>
2022-11-15 13:02:15 +0200

    Updated register form + v3 data

--- src/pages/V1-2.js
+++ src/pages/V1-2.js
@@ -7,6 +7,8 @@ import axios from 'axios';
 function Temperature() {
     const [chartData, setChartData] = useState({})
     const [chartDataV2, setChartDataV2] = useState({})
+    const [chartDataM, setChartDataM] = useState({})
+    const [chartV3, setChartV3] = useState({})
     const [isloading, setisloading] = useState(true)
 
     const V1 = () => {
@@ -51,7 +53,7 @@ function Temperature() {
                     ],
                 })
             })
-            axios.get("http://localhost:8080/v2/climateV2")
+        axios.get("http://localhost:8080/v2/climateV2")
             .then(response => {
                 for (const dataObj of response.data) {
                     yearV2.push(dataObj.year)
@@ -67,10 +69,9 @@ function Temperature() {
                                 '#0CFF00'
                             ]
                         },
-                        
+
                     ],
                 })
-                setisloading(false)
             }).catch(error => {
                 alert(error)
                 setisloading(true)
@@ -79,22 +80,20 @@ function Temperature() {
     }
 
     const V1Monthly = () => {
-        let yearV1 = []
-        let yearV2 = []
+        let year = []
         let anomaly = []
         let anomaly2 = []
         let anomaly3 = []
-        let v2temp = []
-        axios.get("http://localhost:8080/v1/climateV1")
+        axios.get("http://localhost:8080/v1/climateV1monthly")
             .then(response => {
                 for (const dataObj of response.data) {
-                    yearV1.push(dataObj.year)
+                    year.push(dataObj.year)
                     anomaly.push(dataObj.anomaly_g)
                     anomaly2.push(dataObj.anomaly_n)
                     anomaly3.push(dataObj.anomaly_s)
                 }
-                setChartData({
-                    labels: yearV1,
+                setChartDataM({
+                    labels: year,
                     datasets: [
                         {
                             label: 'Maailmanlaajuiset lämpötilapoikkeamat 1850-2022',
@@ -119,26 +118,6 @@ function Temperature() {
                         }
                     ],
                 })
-            })
-            axios.get("http://localhost:8080/v2/climateV2")
-            .then(response => {
-                for (const dataObj of response.data) {
-                    yearV2.push(dataObj.year)
-                    v2temp.push(dataObj._t)
-                }
-                setChartDataV2({
-                    labels: yearV2,
-                    datasets: [
-                        {
-                            label: 'Anders Mobergin et al. Paleoklimatologiset lämpötilatiedot',
-                            data: v2temp,
-                            backgroundColor: [
-                                '#0CFF00'
-                            ]
-                        },
-                        
-                    ],
-                })
                 setisloading(false)
             }).catch(error => {
                 alert(error)
@@ -147,10 +126,40 @@ function Temperature() {
             )
     }
 
-   
+    const V3 = () => {
+        let year = []
+        let mean = []
+        axios.get("http://localhost:8080/v3/climateV3")
+            .then(response => {
+                for (const dataObj of response.data) {
+                    year.push(dataObj.year)
+                    mean.push(dataObj._mean)
+                }
+                setChartV3({
+                    labels: year,
+                    datasets: [
+                        {
+                            label: 'Havaijin Mauna Loan ilmakehän hiilidioksidipitoisuudet',
+                            data: mean,
+                            backgroundColor: [
+                                '#0000FF'
+                            ]
+                        }
+                    ],
+                })
+    
+            }).catch(error => {
+                alert(error)
+                setisloading(true)
+            }
+            )
+    }
+
 
     useEffect(() => {
         V1()
+        V1Monthly()
+        V3()
     }, [])
 
     if (isloading === true) {
@@ -171,17 +180,20 @@ function Temperature() {
                         <div><Line data={chartDataV2} options={{
                             responsive: true,
                         }} /></div>
+                        <div><Line data={chartDataM} options={{
+                            responsive: true,
+                        }} /></div>
                     </div>
                 </div>
                 <div id='chart' style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap' }} className="p-5 mb-4 bg-light rounded-3">
                     <h1>Mauna Loan ilmakehän hiilidioksidipitoisuudet 1959-2021</h1>
                     <div className="container-fluid py-5">
-                        <div><Line data={chartData} options={{
+                        <div><Line data={chartV3} options={{
                             responsive: true,
                         }} /></div>
                     </div>
                 </div>
-                
+
             </>
         )
     }
