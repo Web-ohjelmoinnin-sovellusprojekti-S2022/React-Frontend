6fa9a29df651a43098e0777b225bec620b4acea0
Miekhuovinen <t1humi02@students.oamk.fi>
2022-11-28 13:26:24 +0200

    V8

--- src/pages/Emission.js
+++ src/pages/Emission.js
@@ -3,100 +3,150 @@ import { Bar, Line, Pie } from 'react-chartjs-2';
 import { Chart as ChartJs } from "chart.js/auto";
 import axios from 'axios';
 
+
 const Emission = () => {
   const [chartData, setChartData] = useState({})
+  const [years, setYears] = useState({})
+  const [country, setCountry] = useState([])
+  const [dataset, setDataset] = useState([])
   const [isloading, setisloading] = useState(true)
+  let datasets = []
 
   const V8 = () => {
     let year = []
-    let anomaly = []
+    
     axios.get("http://localhost:8080/v8/climateV8")
       .then(response => {
+        setChartData(response.data)
+        
         for (const dataObj of response.data) {
-          year.push(parseInt(dataObj.year))
-          anomaly.push(dataObj.anomaly)
+        year.push(dataObj.year)
         }
-        setChartData({
-          labels: year,
-          datasets: [
-            {
-              label: 'Anomaly',
-              data: anomaly,
-              backgroundColor: [
-                '#0000FF'
-              ]
-            }
-          ],
-        })
-        setisloading(false)
+       
+        setYears(year)
+       
+        
+      setisloading(false)
       }).catch(error => {
         alert(error)
         setisloading(true)
       }
 
       )
-      const config = {
-        type: 'line',
-        data: chartData,
-        options: {
-          responsive: true,
-          plugins: {
-            title: {
-              display: true,
-              text: (ctx) => 'Chart.js Line Chart - stacked=' + ctx.chart.options.scales.y.stacked
-            },
-            tooltip: {
-              mode: 'index'
-            },
-          },
-          interaction: {
-            mode: 'nearest',
-            axis: 'x',
-            intersect: false
-          },
-          scales: {
-            x: {
-              title: {
-                display: true,
-                text: 'Month'
-              }
-            },
-            y: {
-              stacked: true,
-              title: {
-                display: true,
-                text: 'Value'
-              }
-            }
-          }
-        }
-      };
+   
   }
+  const V8Countries = () => {
+    axios.get("http://localhost:8080/v8/climateV8countries")
+      .then(response => {
+      
+        
+        for (const dataObj of response.data) {
+          setCountry(country => [...country, dataObj.country.replace('\r', '')])
+          }
+          setisloading(false)
+      }).catch(error => {
+        alert(error)
+        setisloading(true)
+      }
+
+      )
+   
+  }
+
+  const countries = country.map(element => {
+    return element.toLowerCase();
+    })
+
+  country.sort()
+  let dynamicColors = function() {
+    let r = Math.floor(Math.random()*255);
+    let g = Math.floor(Math.random()*255);
+    let b = Math.floor(Math.random()*255);
+    return "rgb(" + r + ',' + g + ',' + b + ")";
+  }
+  
+  for (let i = 0; i < country.length; i++) {
+    let coloR = []
+    coloR.push(dynamicColors())
+    const ok = []
+    let text = countries[i].toString()
+
+  for (let j = 0; j < chartData.length; j++){
+    ok.push(chartData[j][text])
+  }
+    datasets.push({
+        label: country[i],
+        data: ok,
+        borderColor: coloR,
+        backgroundColor:  coloR,
+        parsing: {
+          xAxisKey: 'year',
+          yAxisKey: country[i]
+      },
+    })
+  }
+
+  console.log("haloo")
+  console.log(countries)
+
+  const options = {
+    type: 'line',
+    responsive: true,
+    interaction: {
+      mode: 'nearest',
+      axis: 'x',
+      intersect: false
+    },
+    pointRadius: 0,
+    fill: true,
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Year'
+        }
+      },
+      y: {
+        stacked: true,
+        title: {
+          display: true,
+          text: 'Value'
+        }
+      }
+    }
+
+  };
+  let jaa = years.toString()
+  let year = jaa.split(',')
+  const data = {
+    labels: year,
+    datasets: datasets
+  };
+
 
   useEffect(() => {
     V8()
+    V8Countries()
   }, [])
 
-  if(isloading === true){
-    return(
+  if (isloading === true) {
+    return (
       <p>Loading</p>
     )
   }
 
   else {
     return (
-    <>
-      <div id='chart' style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap' }} className="p-5 mb-4 bg-light rounded-3">
-        <div className="container-fluid py-5">
-          <div><Line data={chartData} options={{
-            responsive: true,
-          }} /></div>
+      <>
+        <div id='chart' style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap' }} className="p-5 mb-4 bg-light rounded-3">
+          <div className="container-fluid py-5">
+            <div><Line data={data} options={options}/></div>
+          </div>
         </div>
-      </div>
-    </>
-  )
+      </>
+    )
   }
-  
+
 
 }
 export default Emission
