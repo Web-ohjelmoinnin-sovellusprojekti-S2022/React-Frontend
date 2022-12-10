8c62180405f0b0487d93752ec992dd7669bc0747
t1haju05 <t1haju05@students.oamk.fi>
2022-12-10 12:28:01 +0200

    Navbar scales correctly on mobiles now

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ import Logout from "../pages/Logout";
 
 const NavBar = () => {
 
-    return (
+    /*return (
         <nav className="navbar navbar-expand-lg -navbarlight">
             <a className="navbar-brand" href="/">Gonacompany kaaviot</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,6 +29,31 @@ const NavBar = () => {
                         </ul>
                     </div>
                 </div>
+            </div>
+        </nav>
+    )*/
+
+    return (
+        <nav class="navbar navbar-dark bg-dark navbar-expand-md">
+            <a href="/" class="navbar-brand">Gonacompany kaaviot</a>
+            <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse" id="navbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a href="/about" class="nav-link">Tietoa</a></li>
+                    <li class="nav-item"><a href="/logout" class="nav-link">Kirjaudu ulos</a></li>
+                    <div className="dropdown">
+                        <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                            Kaaviot
+                        </a>
+                        <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink" id='pasiviheraho'>
+                            <li><a className="dropdown-item" href="/globaltemp">Lämpötilakaaviot</a></li>
+                            <li><a className="dropdown-item" href="/emission">Päästökaaviot</a></li>
+                            <li><a className="dropdown-item" href="/customview">Luo oma näkymä</a></li>
+                        </ul>
+                    </div>
+                </ul>
             </div>
         </nav>
     )
